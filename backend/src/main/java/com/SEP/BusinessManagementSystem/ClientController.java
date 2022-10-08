fd9c831b8aa5b28003ea618d389601d5fba430d2
eliooreuxx <luhaida13172@gmail.com>
2022-10-08 20:29:29 +0200

    Implement a get api endpoint which returns a demo client

---
+++ backend/src/main/java/com/SEP/BusinessManagementSystem/ClientController.java
@@ -0,0 +1,24 @@
+package com.SEP.BusinessManagementSystem;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/getAllClients")
+public class ClientController {
+
+    @GetMapping
+    public List<Client> getAllClients() {
+        return List.of(
+                new Client(
+                        1,
+                        "Ivar Jacobson",
+                        "070 123 4567"
+                )
+        );
+    }
+}

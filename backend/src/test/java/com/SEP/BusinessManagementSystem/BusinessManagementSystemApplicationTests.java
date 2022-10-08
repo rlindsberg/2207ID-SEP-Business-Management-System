6b5f1e4e2930cdf74107bd201d44b31297a51b78
eliooreuxx <luhaida13172@gmail.com>
2022-10-08 19:41:24 +0200

    initializer

---
+++ backend/src/test/java/com/SEP/BusinessManagementSystem/BusinessManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SEP.BusinessManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

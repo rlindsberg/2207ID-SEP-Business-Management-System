c5b0e64d6b1b5303972674aeb7dc60af82896c34
eliooreuxx <luhaida13172@gmail.com>
2022-10-08 20:28:54 +0200

    Implement the Client class with Constructor, getter and setters

---
+++ backend/src/main/java/com/SEP/BusinessManagementSystem/Client.java
@@ -0,0 +1,43 @@
+package com.SEP.BusinessManagementSystem;
+import javax.persistence.*;
+
+@Entity
+public class Client {
+    @Id
+    private int id;
+    private String name;
+    private String phoneNumber;
+
+    public Client() {
+    }
+
+    public Client(int id, String name, String phoneNumber) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}

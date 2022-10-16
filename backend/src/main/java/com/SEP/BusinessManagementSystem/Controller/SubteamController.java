1bbd0dcdfa6ff77ae0996082a9f5fc3eaf95ae9d
eliooreuxx <luhaida13172@gmail.com>
2022-10-16 16:02:27 +0200

    Implement initial subteam classes. Can create a new subteam (not sure if it is necessary) May need to consider more methods.

---
+++ backend/src/main/java/com/SEP/BusinessManagementSystem/Controller/SubteamController.java
@@ -0,0 +1,45 @@
+package com.SEP.BusinessManagementSystem.Controller;
+
+import com.SEP.BusinessManagementSystem.Entity.Request;
+import com.SEP.BusinessManagementSystem.Entity.Subteam;
+import com.SEP.BusinessManagementSystem.Entity.Task;
+import com.SEP.BusinessManagementSystem.Service.SubteamService;
+import com.SEP.BusinessManagementSystem.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/v1/subteams")
+public class SubteamController {
+    private final SubteamService subteamService;
+
+    @Autowired
+    public SubteamController(SubteamService subteamService) {
+        this.subteamService = subteamService;
+    }
+
+
+    @GetMapping("/get/{Id}")
+    public ResponseEntity<?> getSubteamById(@PathVariable Long Id){
+        return ResponseEntity.ok().body(subteamService.getSubteamById(Id));
+    }
+
+//    @GetMapping("/get-all")
+//    public List<Subteam> getAllSubteams() {
+//        return subteamService.getAllSubteams();
+//    }
+//
+
+    // do we need to create a subteam?
+    @PostMapping("/create/{Id}")
+    public ResponseEntity<?> createNewSubteam(@PathVariable Long Id, @RequestBody Subteam subteamDetails) {
+        URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("subteams/create").toUriString());
+        return ResponseEntity.created(uri).body(subteamService.createNewSubteam(Id, subteamDetails));
+    }
+}

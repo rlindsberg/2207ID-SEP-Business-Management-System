9699590b854299d1eb12ea4a2aa3b5e0ef41f4ef
Roderick Karlemstrand <roderick@karlemstrand.com>
2022-11-03 22:11:26 +0100

    7. add SecurityConfigurer

---
+++ backend/src/main/java/com/SEP/BusinessManagementSystem/Security/SecurityConfigurer.java
@@ -0,0 +1,60 @@
+package com.SEP.BusinessManagementSystem.Security;
+
+import com.SEP.BusinessManagementSystem.Service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private MyUserDetailsService myUserDetailsService;
+
+    @Autowired
+    JwtRequestFilter jwtRequestFilter;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate").permitAll()
+
+                .antMatchers("/clients/**").hasAnyAuthority("ROLE_CUSTOMER_SERVICE")
+                .antMatchers("/requests/create").hasAnyAuthority("ROLE_CUSTOMER_SERVICE")
+                .antMatchers("/requests/**").hasAnyAuthority("ROLE_CUSTOMER_SERVICE", "ROLE_FINANCIAL", "ROLE_ADMIN")
+                .antMatchers("/events/**").authenticated()
+                .antMatchers("/tasks/**").authenticated()
+                .antMatchers("/subteams/**").hasAnyAuthority("ROLE_ADMIN")
+
+                .anyRequest().authenticated()
+                
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
